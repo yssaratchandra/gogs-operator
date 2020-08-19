2fb19123acf4ac67cc0445afd749604172865bdb
sasratchandrays <emailads.market@gmail.com>
2020-08-19 12:14:44 -0500

    checking scripts

---
+++ scripts/setup_gogs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage:"
+    echo "  $0 GUID"
+    exit 1
+fi
+
+GUID=$1
+oc new-project $GUID-gogs --display-name="Gogs"
+
+oc apply -f ./deploy/service_account.yaml
+oc apply -f ./deploy/role.yaml
+oc apply -f ./deploy/role_binding.yaml
+oc apply -f ./deploy/operator.yaml
+oc apply -f ./deploy/gogs.yaml
+sleep 60
+oc patch deployment postgresql-gogs-gogs --patch='{ "spec": { "template": { "spec": { "securityContext": { "fsGroup": 2000 }}}}}'
+
